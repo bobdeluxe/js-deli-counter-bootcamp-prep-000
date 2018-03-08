6c5c4de1b9172637f14350ad975c23aa13f643e9
bob deluxe <learncobobdeluxe@gmail.com>
2018-03-08 16:00:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,9 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  
+  if(katzDeliLine===0) {
+    return "The line is currently empty."
+  } else {
+    for()
+  }
 }
