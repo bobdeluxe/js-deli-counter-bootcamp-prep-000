8c882707e40401daf232ec12e508ba0b1c2341f2
bob deluxe <learncobobdeluxe@gmail.com>
2018-03-08 16:02:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if(katzDeliLine===0) {
+  if(katzDeliLine.length===0) {
     return "The line is currently empty."
   } else {
     return katzDeliLine[0]
