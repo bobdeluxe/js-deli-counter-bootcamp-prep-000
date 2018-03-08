2b45e6d940409cad44b272148f3139d7782559fc
bob deluxe <learncobobdeluxe@gmail.com>
2018-03-08 15:35:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function takeANumber(katzDeliLine,name) {
   return katzDeliLine.push(name).length+1
 }
+
+function nowServing() {
+  if(katzDeli.length===0) {
+    "There is noboby waiting to be served!"
+  } else {
+    return
+  }
+}
