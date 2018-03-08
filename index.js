31b8ecec6e91d67786b956528642934f125d57d2
bob deluxe <learncobobdeluxe@gmail.com>
2018-03-08 16:25:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
   } else {
     var line = "The line is currently:"
     for(var i=0;i<katzDeliLine.length;i++) {
-      if(katzDeliLine.length===0) {
+      if(katzDeliLine.length===katzDeliLine.length) {
         line+=` ${i+1}. ${katzDeliLine[i]}`
       } else {
         line+=` ${i+1}. ${katzDeliLine[i]},`
